d50264ff13d6c37fe19f83323be6fccc7e5efdb0
Elena.y0 <elena.yordanova89@gmail.com>
2024-04-18 19:37:27 +0300

    changes

---
+++ .vscode/lab13/loops.py
@@ -0,0 +1,7 @@
+1,2,3,4,5
+i=1
+total sum=0
+while i<=5:
+    total_sum+=i
+
+print('total_sum=', total_sum)
--- loops.py
+++
@@ -1,5 +0,0 @@
-x=input ('Enter even number:')
-while x%2==0:
-    x=int(input('Enter even number:')))
-
-print (x)
