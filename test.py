8d2b1cb950232d20f41a9725add3918ff1328c1d
Elena.y0 <elena.yordanova89@gmail.com>
2024-04-18 12:41:59 +0300

    save changes

---
+++ test.py
@@ -0,0 +1,16 @@
+# print(2+2)
+# print (True or 2645473)
+
+
+
+# user_name = 'ada'
+# user_name= user_name or 'Anonymous'
+
+# print (user_name) 
+
+# if -1:
+  #  print ('hi')
+
+bmi = 12.0
+if bmi<16.0:
+    print ('underweight thinness')
